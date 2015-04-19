dd8a5a12a5b7fc7a49b0a2ca72423ae47a28307c
amitk <amitk@wix.com>
2015-04-19 21:12:30 +0300

    Returned the needed text plugin for jsx on the fly compilation

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ requirejs.config({
     paths: {
         lodash: 'bower_components/lodash/dist/lodash.min',
         react: 'bower_components/react/react-with-addons',
+        text: 'bower_components/requirejs-text/text',
         domReady: 'bower_components/requirejs-domready/domReady',
         jsx: 'lib/jsx',
         JSXTransformer: 'lib/JSXTransformer'
@@ -11,9 +12,9 @@ requirejs.config({
     }
 });
 
-requirejs(['jquery', 'react', 'jsx!app'], function ($, React, app) {
+requirejs(['react', 'jsx!app'], function (React, app) {
     require(['domReady!'], function (document) {
         /*eslint new-cap:0*/
-        React.render(React.createElement(app), $('#main').get(0));
+        React.render(React.createElement(app), document.getElementById('main'));
     });
 });
